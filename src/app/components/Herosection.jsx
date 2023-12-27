a0afad7ce1a174a7074f211b4bdcc7d4df425053
ashish garg <ashish.garg@metamerch.in>
2023-12-27 20:01:47 +0530

    added resume

--- src/app/components/Herosection.jsx
+++ src/app/components/Herosection.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Herosection() {
@@ -8,7 +9,9 @@ export default function Herosection() {
                 <h4 className='text-lg lg:text-xl text-white'>Hi There! 👋🏻</h4>
                 <h2 className='text-4xl lg:text-6xl font-extrabold mt-4 mb-3 stylefont text-white'>I'M Ashish garg</h2>
                 <h3 className='text-white text-lg mb-4 '>I am a React Developer</h3>
-                <button className='donwloadButton text-white'>Donwload Resume</button>
+                <Link href={"./ashishgarg.pdf"}>
+                <button  className='donwloadButton text-white'>Donwload Resume</button>
+                </Link>
             </div>
         </div>
     </>
