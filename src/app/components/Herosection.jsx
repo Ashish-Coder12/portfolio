c2570d15ae7271c6546b0855107f7966c052c048
ashish garg <ashish.garg@metamerch.in>
2023-12-27 15:53:34 +0530

    added black color

--- src/app/components/Herosection.jsx
+++ src/app/components/Herosection.jsx
@@ -5,10 +5,10 @@ export default function Herosection() {
    <>
         <div className='setbackground flex items-center pl-5'>
             <div className='custompadding'>
-                <h4 className='text-lg lg:text-xl'>Hi There! 👋🏻</h4>
-                <h2 className='text-4xl lg:text-6xl font-extrabold mt-4 mb-3 stylefont'>I'M Ashish garg</h2>
+                <h4 className='text-lg lg:text-xl text-white'>Hi There! 👋🏻</h4>
+                <h2 className='text-4xl lg:text-6xl font-extrabold mt-4 mb-3 stylefont text-white'>I'M Ashish garg</h2>
                 <h3 className='text-white text-lg mb-4 '>I am a React Developer</h3>
-                <button className='donwloadButton'>Donwload Resume</button>
+                <button className='donwloadButton text-white'>Donwload Resume</button>
             </div>
         </div>
     </>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 export default function Navbar() {
   return (
     <nav >
-      <h1 className='logo ml-5'>Ashish </h1>
-      <ul className='links'>
-        <li className="navlinks">Home</li>
-        <li className="navlinks">Details</li>
-        <li className="navlinks">Projects</li>
-        <li className="navlinks">Contact</li>
-        <button className='linkbutton'>Resume</button>
+      <h1 className='logo ml-5 text-white'>Ashish </h1>
+      <ul className='links '>
+        <li className="navlinks text-white">Home</li>
+        <li className="navlinks text-white">Details</li>
+        <li className="navlinks text-white">Projects</li>
+        <li className="navlinks text-white">Contact</li>
+        <button className='linkbutton text-white'>Resume</button>
       </ul>
     </nav>
   )
