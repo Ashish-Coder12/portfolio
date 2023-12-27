bc9df4da84722babff70e20647c2b82f054e377a
ashish garg <ashish.garg@metamerch.in>
2023-12-27 08:27:49 +0530

    initial commit

---
+++ src/app/components/Herosection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Herosection() {
+  return (
+    <div>
+        <h4 className='text-lg lg:text-xl'>Lets Code the </h4>
+        <h2 className='text-4xl lg:text-6xl font-extrabold'>Future</h2>
+        <h3 className='text-white mb-4 '>Hii, I am Ashish Garg</h3>
+
+    </div>
+  )
+}
